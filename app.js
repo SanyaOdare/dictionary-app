d7e3055c3ccb1d9389e4c0b11632cc7d6d3b64c2
SanyaOdare <sanyaodare@gmail.com>
2021-12-28 14:39:53 +0100

    import google font

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const wrapper = document.querySelector(".wrapper"),
+searchInput = wrapper.querySelector("input"),
+volume = wrapper.querySelector(".word i"),
+infoText = wrapper.querySelector(".info-text"),
+synonyms = wrapper.querySelector(".synonyms .list"),
+removeIcon = wrapper.querySelector(".search span");
 
+let audio;
+
+function data(result, word) {
+  if (result.title) {
+    infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+  } else {
+    wrapper.classList.add("active");
+    let definitions = result[0].meanings[0].definitions[0],
+    phonetics = `${result[0].meanings[0].partOfSpeech}/${result[0].phonetics[0].text}/`;
+    document.querySelector(".word p").innerText = result[0].word;
+    document.querySelector(".word span").innerText = phonetics;
+  }
+}
